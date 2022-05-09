9ee985186dcacaa1526eea7a4bde27c4d4e3fbd5
Jordy Gracia <graciamancilla@icloud.com>
2022-05-09 04:59:34 -0500

    se agrega archivo que se elimina por error

---
+++ PruebaiOS/Extensions/ColorTableExtension.swift
@@ -0,0 +1,35 @@
+//
+//  ColorTableExtension.swift
+//  PruebaiOS
+//
+//  Created by Jordy Gracia on 09/05/22.
+//
+
+import Foundation
+import UIKit
+
+extension ColorFavoritoCell: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        colores.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = colores[indexPath.row]
+        return cell
+    }
+    
+    
+}
+extension ColorFavoritoCell: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = ColorTable.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = ColorTable.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+    }
+}
+}
